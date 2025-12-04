439d4e9a2cbc0984a9d966da482feb5a85ecf809
Bubai Ghosh <bghosh482@gmail.com>
2025-12-05 00:49:19 +0530

    added security class and getdata method

---
+++ cards/src/main/java/com/eazybytes/cards/security/SecurityController.java
@@ -0,0 +1,11 @@
+package com.eazybytes.cards.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityController {
+    public String getData(){
+        // some code
+        return "";
+    }
+}
